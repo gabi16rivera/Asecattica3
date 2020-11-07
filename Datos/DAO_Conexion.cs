882af658fd8f347f3c25225ec94e0c8dbd72078d
gabi16rivera <73187106+gabi16rivera@users.noreply.github.com>
2020-11-07 14:47:34 -0600

    Datos

---
+++ Datos/DAO_Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class DAO_Conexion
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=DESKTOP-27BUFAE;DataBase=bdAsecattica;Integrated Security=true");
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+
+
+    }
+}
---
+++ Datos/DAO_Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class DAO_Login
+    {
+        private DAO_Conexion conexion = new DAO_Conexion();
+        public int ValidarLogin(string cedula,string clave)
+        {
+            SqlCommand comandoActUsu = new SqlCommand();
+            comandoActUsu.Connection = conexion.AbrirConexion();
+            comandoActUsu.CommandText = "sp_crud_TBUsuarios";
+            comandoActUsu.CommandType = CommandType.StoredProcedure;
+            comandoActUsu.Parameters.AddWithValue("@choice", "VALIDAR_LOGIN");
+            comandoActUsu.Parameters.AddWithValue("@Cedula", cedula);
+            comandoActUsu.Parameters.AddWithValue("@clave", clave);
+
+            return (int)comandoActUsu.ExecuteScalar();
+
+        }//validar login
+
+
+    }
+}
